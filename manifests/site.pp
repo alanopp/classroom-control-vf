61ba51c7de8fda11e179ad3a2bae79d4868afba7
alanopp <alopp@multiservice.com>
2016-10-25 14:09:18 -0500

    Update site.pp
    Change motd

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,10 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
+  
+ file {'/etc/motd':
+   ensure  => file,
+   owner   => 'root'
+   content => "Look what puppet can do!\n",
+ }
 }
