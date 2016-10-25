713ee830e35e160386040457c5d4a58d06d34126
alanopp <alopp@multiservice.com>
2016-10-25 15:03:03 -0500

    fix error

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,7 +42,7 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  include role::
+  include role::classroom
   exec { 'motd':
     path    => '/usr/bin',
     creates => "cowsay 'Welcome to alanopp' > /etc/motd",
