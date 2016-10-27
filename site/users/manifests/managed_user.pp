fa9facf470f6c691f0e3c93c7b126cd0f74e7316
alanopp <alopp@multiservice.com>
2016-10-27 15:17:28 -0500

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,12 +1,25 @@
+# users/manifests/managed_user.pp
 define users::managed_user (
   $group = $title,
 ) {
   user { $title:
-    ensure => present,
+    ensure     => present,
+    managehome => true,
   }
   file { "/home/${title}":
     ensure => directory,
     owner => $title,
     group => $group,
   }
+  file { "/home/${title}/.ssh":
+    ensure => directory,
+    owner => $title,
+    mode  => '0700',
+  }
+   file { "/home/${title}/.bashrc":
+    ensure => file,
+    owner => $title,
+    mode  => '0644'
+    source => 'puppet:///modules/users/bashrc',
+   }
 }
