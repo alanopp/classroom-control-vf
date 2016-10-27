55ed5d82cb78a54a0e66b3a4aacd84e74df1791e
alanopp <alopp@multiservice.com>
2016-10-27 10:01:36 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,9 +1,10 @@
 # manifests/init.pp
 class nginx {
   File {
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0664',
   }
 
   package { 'nginx':
@@ -15,22 +16,19 @@ class nginx {
   }
   
   file { '/var/www/index.html':
-    ensure => file,
     source => 'puppet:///modules/nginx/index.html',
   }
   
   file { '/etc/nginx/nginx.conf':
-    ensure => file,
     source => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
     notify => Service['nginx'],
   }
   
   file { '/etc/nginx/conf.d/default.conf':
-    ensure => file,
    source => 'puppet:///modules/nginx/default.conf',
    notify => Service['nginx'],
-    require => Package['nginx'],
+   require => Package['nginx'],
   }
 
   service { 'nginx':
